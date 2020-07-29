cbab4cca2ec3520d255321b062d12c500d156319
Andy Ta <me.andyta@gmail.com>
2020-07-28 23:23:14 -0400

    Initial commit

---
+++ src/test/kotlin/teams/upnext/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package teams.upnext.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
